43d75096cf61e43230ddc725ccc54bb7bb8c4ee5
ymlisn <ymclins@gmail.com>
2020-11-03 22:37:22 -0300

    Primeiro Commit

---
+++ src/test/java/com/projeto/noteapp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.noteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
